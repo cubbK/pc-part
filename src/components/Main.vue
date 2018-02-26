e189d6f644c15313fcdb284c2c14c926904c071c
Dan <danpopa1998@gmail.com>
2018-02-27 01:32:31 +0200

    add toppings

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,11 +1,22 @@
 <template>
   <div class="main-container">
     <img src="./main/pizza-pixel-art-square.png" alt="">
+    <img src="./main/lettuce.png" alt="" class="pizza-topping">
+    <img src="./main/mushrooms.png" alt="" class="pizza-topping">
+    <img src="./main/olives.png" alt="" class="pizza-topping">
   </div>
 </template>
 
 <style scoped>
   .main-container img{
     max-width: 100%;
+  }
+
+  .pizza-topping {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    margin: auto;
   }
 </style>
