9f066747ca95efe0d02c1047d41b29bb42ce4357
Dan <danpopa1998@gmail.com>
2018-02-26 23:36:42 +0200

    add pizza image to main component

---
+++ src/components/Main.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="main-container">
+    <img src="./main/pizza-pixel-art-square.png" alt="">
+  </div>
+</template>
+
+<style scoped>
+  .main-container img{
+    max-width: 100%;
+  }
+</style>
