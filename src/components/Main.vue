f57a7d32e94d0031516246acf18b3e370ebc1533
Dan <danpopa1998@gmail.com>
2018-03-01 09:30:55 +0200

    start adding rpgui

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,22 +1,25 @@
 <template>
-  <div class="main-container">
-    <img src="./main/pizza-pixel-art-square.png" alt="">
+  <div class="main-container rpgui-content">
+    <!-- <img src="./main/pizza-pixel-art-square.png" alt="">
     <img src="./main/lettuce.png" alt="" class="pizza-topping">
     <img src="./main/mushrooms.png" alt="" class="pizza-topping">
-    <img src="./main/olives.png" alt="" class="pizza-topping">
+    <img src="./main/olives.png" alt="" class="pizza-topping"> -->
+    <div class="rpgui-container framed">
+      123asdd
+    </div>
   </div>
 </template>
 
 <style scoped>
-  .main-container img{
-    max-width: 100%;
-  }
+.main-container img {
+  max-width: 100%;
+}
 
-  .pizza-topping {
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    margin: auto;
-  }
+.pizza-topping {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
 </style>
