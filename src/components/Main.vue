643f80786b69dec2eaf452344a9b95f42378a6e4
Dan <danpopa1998@gmail.com>
2018-03-01 23:09:27 +0200

    add rpgui images & add options

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="main-container rpgui-content">
-    <!-- <img src="./main/pizza-pixel-art-square.png" alt="">
-    <img src="./main/lettuce.png" alt="" class="pizza-topping">
-    <img src="./main/mushrooms.png" alt="" class="pizza-topping">
-    <img src="./main/olives.png" alt="" class="pizza-topping"> -->
-    <div class="rpgui-container framed">
-      123asdd
+    <div class="rpgui-container framed pizza-container">
+      <div class="pizza">
+        <img src="./main/pizza-pixel-art-square.png" alt="">
+        <img src="./main/lettuce.png" alt="" class="pizza-topping">
+        <img src="./main/mushrooms.png" alt="" class="pizza-topping">
+        <img src="./main/olives.png" alt="" class="pizza-topping">
+      </div>
+      <hr class="golden">
+      <div class="pizza-options">
+        <div class="pizza-type">
+        <div class="pizza-type-label">
+          Type
+        </div>
+        <select class="rpgui-dropdown">
+          <option value="option1">Neapolitana</option>
+          <option value="option2">option2</option>
+        </select>
+      </div>
+      <div class="order">
+        <button class="rpgui-button" type="button"><p>Order</p></button>
+      </div>
+      </div>
+
     </div>
   </div>
 </template>
 
 <style scoped>
+.pizza-container {
+  padding-top: 0;
+  padding-left: 0;
+  padding-right: 0;
+}
+
 .main-container img {
   max-width: 100%;
+}
+
+.pizza {
+  position: relative;
+}
+
+.pizza-options {
+  padding: 0 10px;
 }
 
 .pizza-topping {
@@ -21,5 +52,18 @@
   left: 0;
   right: 0;
   margin: auto;
+}
+
+.pizza-type {
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+
+.pizza-type-label {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-grow: 1;
 }
 </style>
